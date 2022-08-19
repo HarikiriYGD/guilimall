911a5c8774025a2f4c3b436b352c03c8bb9ed6a8
LilBoat <GuodanYang_nn@163.com>
2022-08-19 17:04:12 +0800

    谷粒商城CRUD的快速开发2

---
+++ guilimall-gateway/src/test/java/com/swjtu/guilimall/gateway/GuilimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swjtu.guilimall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
